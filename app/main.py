2af0ad6159907d3368e229aaafb0b517d8b07cb3
Evan <evanngregorio@gmail.com>
2026-03-04 12:28:29 +0000

    Connected FASTAPI to PostgreSQL on AWS EC2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+<<<<<<< HEAD
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on AWS!"}
+=======
+from .database import engine
+from .models import Base
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def read_root():
+	return {"message": "Hello from FastAPI on AWS!"}
+>>>>>>> 2e7469f (Connected -m Connected FastAPI to PostgreSQL on AWS EC2)
